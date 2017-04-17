78daf1fd75ef848d81a41e04005fde7c29dec03d
ikoshcheiev <koshcheiev.vanya@gmail.com>
2017-04-17 22:59:48 +0300

    second one

--- chapter1/src/test/groovy/com/manning/spock/chapter1/AdderSpec.groovy
+++ chapter1/src/test/groovy/com/manning/spock/chapter1/AdderSpec.groovy
@@ -7,7 +7,7 @@ class AdderSpec extends spock.lang.Specification{
 		when: "a new Adder class is created"
 		def adder = new Adder();
 
-		then: "1 plus 1 should be 2"
+		then: "1 plus 1 is 2"
 		adder.add(1, 1) == 2
 	}
 	
